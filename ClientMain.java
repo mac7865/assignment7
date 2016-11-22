1fc83e749379d2541cfbbf74325f812cd693a08e
Mark Carter <Mark@wireless-10-147-72-246.public.utexas.edu>
2016-11-22 12:50:53 -0600

    init commit, adding base files

---
+++ ClientMain.java
@@ -0,0 +1,18 @@
+package assignment7;
+
+/* ChatRoom ClientMain.java
+ * EE422C Project 7 submission by
+ * Mark Carter
+ * mac7865
+ * 16495
+ * Slip days used: 0
+ * Fall 2016
+ */
+
+public class ClientMain {
+
+	public static void main(String[] args) {
+
+	}
+
+}
---
+++ ServerMain.java
@@ -0,0 +1,18 @@
+package assignment7;
+
+/* ChatRoom ServerMain.java
+ * EE422C Project 7 submission by
+ * Mark Carter
+ * mac7865
+ * 16495
+ * Slip days used: 0
+ * Fall 2016
+ */
+
+public class ServerMain {
+
+	public static void main(String[] args) {
+
+	}
+
+}
